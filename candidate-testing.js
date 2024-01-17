57e3ea6af1ad8c1262c8163ffdf885e5911b34f1
Tim-E <caffeinated.ink@gmail.com>
2024-01-17 13:04:36 -0500

    Passed tests 1 - 10

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,7 +13,7 @@ let candidateAnswer = "";
 
 //TODO: Variables for Part 2
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
-let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", 3];
+let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
 let candidateAnswers = [];
 
 
@@ -23,10 +23,10 @@ function askForName() {
   return candidateName;
 }
 
-function askQuestion() {
+function askQuestion(questions) {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  for (let i = 0; i < correctAnswer.length; i++) {
-    candidateAnswers[i] = input.question(questions[i]);
+  for (const question in questions) {
+    candidateAnswers[question] = input.question(questions[question]);
   }
 }
 
@@ -45,6 +45,9 @@ function gradeQuiz(candidateAnswers) {
 
   return grade;
 }
+
+askQuestion(questions);
+console.log(candidateAnswers);
 
 function runProgram() {
   askForName();
